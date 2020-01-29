238ca31832d1e8afdbb8acc8d1860b1eb2ce3a4a
Dmitry Kharitonov <geakstr@me.com>
2020-01-30 00:01:51 +0400

    upgrade node to 13.7 and npm packages

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:12.13-slim
+FROM node:13.7-slim
 
 ENV CI true
 
--- client/src/components/TroubleshootingView/state/actions.ts
+++ client/src/components/TroubleshootingView/state/actions.ts
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-import { PolicySpecs, PolicySpecsUserArgs } from "../../../graphqlTypes";
+import { PolicySpecs, UserPolicySpecsArgs } from "../../../graphqlTypes";
 import { createAsyncAction } from "../../../state";
 import { GqlResult } from "../../App/state/types";
 import * as queries from "./queries";
@@ -19,7 +19,7 @@ import * as queries from "./queries";
 export const fetchPolicySpecs = createAsyncAction({
   name: "Fetch Policy Specs",
   action: async (
-    args: PolicySpecsUserArgs,
+    args: UserPolicySpecsArgs,
     { client }
   ): Promise<PolicySpecs[]> => {
     type ResultType = GqlResult<{ policySpecs: PolicySpecs[] }>;
