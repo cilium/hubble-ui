a958ebc32323b90a3fc7d90a10490221e1dd55f7
Dmitry Kharitonov <geakstr@me.com>
2020-04-27 13:15:48 +0400

    deps: upgrade hubble and other deps
    Signed-off-by: Dmitry Kharitonov <dmitry@isovalent.com>

--- common/generate-types.sh
+++ common/generate-types.sh
@@ -7,15 +7,15 @@ mkdir -p $OUT_DIR
 
 for proto_dir in observer flow; do
   grpc_tools_node_protoc \
-    -I ${CWD}/../hubble/api/v1/ \
+    -I ${CWD}/../hubble/vendor/github.com/cilium/cilium/api/v1/ \
     --js_out=import_style=commonjs,binary:${OUT_DIR} \
     --grpc_out=${OUT_DIR} \
     --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
-    ${CWD}/../hubble/api/v1/$proto_dir/*.proto
+    ${CWD}/../hubble/vendor/github.com/cilium/cilium/api/v1/$proto_dir/*.proto
 
   grpc_tools_node_protoc \
-    -I ${CWD}/../hubble/api/v1/ \
+    -I ${CWD}/../hubble/vendor/github.com/cilium/cilium/api/v1/ \
     --plugin="protoc-gen-ts=${CWD}/node_modules/grpc_tools_node_protoc_ts/bin/protoc-gen-ts" \
     --ts_out=${OUT_DIR} \
-    ${CWD}/../hubble/api/v1/$proto_dir/*.proto
+    ${CWD}/../hubble/vendor/github.com/cilium/cilium/api/v1/$proto_dir/*.proto
 done
