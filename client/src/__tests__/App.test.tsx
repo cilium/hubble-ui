9250f3d47a20c5659a0cce1a0fbca8d52adf1ebb
Renat Tuktarov <yandzeek@gmail.com>
2020-05-06 17:30:00 +0300

    fix: module mocking removed from test-utils
    Signed-off-by: Renat Tuktarov <renat@isovalent.com>

--- client/src/__tests__/App.test.tsx
+++ client/src/__tests__/App.test.tsx
@@ -1,6 +1,8 @@
 import { act, React, render } from '@test-utils';
-import api from '~/api';
+import api from '~/api/grpc';
 import { App } from '~/components/App';
+
+jest.mock('~/api/grpc');
 
 test('App renders without exceptions', async () => {
   await act(async () => {
