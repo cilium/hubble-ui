180332c03ed603fb7d481875fe0e541d2bb8ca95
Renat Tuktarov <yandzeek@gmail.com>
2020-05-06 17:30:00 +0300

    fix: module mocking removed from test-utils
    Signed-off-by: Renat Tuktarov <renat@isovalent.com>

--- client/src/utils/@test-utils.tsx
+++ client/src/utils/@test-utils.tsx
@@ -2,9 +2,6 @@ import { render } from '@testing-library/react';
 import React, { FunctionComponent } from 'react';
 import { StoreProvider } from '~/store';
 
-jest.mock('~/api/grpc');
-jest.mock('~/api/flows-stream');
-
 const AllProviders: FunctionComponent = ({ children }) => {
   return <StoreProvider>{children}</StoreProvider>;
 };
