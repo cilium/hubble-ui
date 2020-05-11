ab5fc81f2e7276ff3d92c26bd954790f3663792b
Dmitry Kharitonov <geakstr@me.com>
2020-05-11 21:19:00 +0400

    tests: added basic tests for flows table sidebar
    Signed-off-by: Dmitry Kharitonov <dmitry@isovalent.com>

---
+++ client/src/components/FlowsTable/__tests__/Sidebar.test.tsx
@@ -0,0 +1,132 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { FlowsTableSidebar } from '~/components/FlowsTable/Sidebar';
+import { Flow } from '~/domain/flows';
+import { HubbleFlow } from '~/domain/hubble';
+import { act, data, fireEvent, React, render } from '~/testing';
+
+interface Expectations {
+  title: string;
+  // if `body` is omited than test expects no related `title` to be found
+  body?: string;
+}
+
+const findByTextContent = (elements: NodeListOf<Element>, text: string) => {
+  for (let i = 0; i < elements.length; i++) {
+    const el = elements[i];
+    if (el.textContent === text) {
+      return el;
+    }
+  }
+  return;
+};
+
+const renderSidebar = (component: React.ReactElement<any>): HTMLElement => {
+  let container: HTMLElement | null = null;
+
+  act(() => {
+    container = render(component).container;
+  });
+
+  return container!;
+};
+
+const runInteractionTests = (
+  container: HTMLElement,
+  onClose: jest.Mock<void>,
+) => {
+  fireEvent.click(container.querySelector(`.close`)!);
+  expect(onClose.mock.calls.length).toBe(1);
+};
+
+const runAppearanceTests = (container: HTMLElement, exps: Expectations) => {
+  const titles = container!.querySelectorAll(`.title`)!;
+  const titleElement = findByTextContent(titles, exps.title);
+
+  if (exps.body) {
+    expect(titleElement).toBeDefined();
+    expect(titleElement!.textContent).toBe(exps.title);
+
+    const bodyElement = titleElement!.nextElementSibling;
+    expect(bodyElement).toBeDefined();
+    expect(bodyElement!.textContent).toBe(exps.body);
+  } else {
+    expect(titleElement).toBeUndefined();
+  }
+};
+
+// 1. Testing that sidebar renders (or not) related titles and flow info
+// 2. Testing that element under `.close` is clickable
+const runTest = (ntest: number, hf: HubbleFlow, exps: Expectations) => {
+  const flow = new Flow(hf);
+
+  const onClose = jest.fn(() => void 0);
+  describe(`Sidebar: test ${ntest}`, () => {
+    let container: HTMLElement | null = null;
+
+    beforeEach(() => {
+      container = renderSidebar(
+        <FlowsTableSidebar flow={flow} onClose={onClose}></FlowsTableSidebar>,
+      );
+    });
+
+    test(`visual`, () => {
+      runAppearanceTests(container!, exps);
+    });
+
+    test(`interactions`, () => {
+      runInteractionTests(container!, onClose);
+    });
+  });
+};
+
+// `Direction` info renders
+runTest(1, data.flows.hubbleOne, {
+  title: 'Direction',
+  body: 'request',
+});
+
+// `Verdict` info renders
+runTest(2, data.flows.hubbleOne, {
+  title: 'Verdict',
+  body: 'forwarded',
+});
+
+// `Source pod` info renders
+runTest(3, data.flows.hubbleOne, {
+  title: 'Source pod',
+  body: 'sender-a1b2c3',
+});
+
+// `Destination pod` info renders
+runTest(4, data.flows.hubbleOne, {
+  title: 'Destination pod',
+  body: 'receiver-d4e5f6',
+});
+
+// `Source labels` info renders
+runTest(5, data.flows.hubbleOne, {
+  title: 'Source labels',
+  body: 'app=Sender namespace=SenderNs',
+});
+
+// `Destination labels` info renders
+runTest(6, data.flows.hubbleOne, {
+  title: 'Destination labels',
+  body: 'app=Receiver namespace=ReceiverNs',
+});
+
+// `Destination labels` info renders
+runTest(7, data.flows.hubbleOne, {
+  title: 'Destination port',
+  body: '80',
+});
+
+// `Destination DNS` info doesn't render when unavailable
+runTest(8, data.flows.hubbleOne, {
+  title: 'Destination DNS',
+});
+
+// `Destination IP` info doesn't render when unavailable
+runTest(9, data.flows.hubbleOne, {
+  title: 'Destination IP',
+});
