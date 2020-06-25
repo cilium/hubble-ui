e216d672bdd4b9d1511485c0a948b217ba33702f
Dmitry Kharitonov <geakstr@me.com>
2020-06-25 19:43:16 +0400

    feat: combine service connectors if possible
    Signed-off-by: Dmitry Kharitonov <dmitry@isovalent.com>
    Signed-off-by: Renat Tuktarov <renat@isovalent.com>

--- client/src/domain/ids.ts
+++ client/src/domain/ids.ts
@@ -2,4 +2,12 @@ export const ids = {
   accessPoint: (cardId: string, port: number): string => {
     return `ap-${cardId}-${port}`;
   },
+  cardConnector: (
+    cardId: string,
+    accessPointIds: Set<string> | string[],
+  ): string => {
+    const accessPointsId = Array.from(accessPointIds).sort().join('/');
+
+    return `cnctr-${cardId}-(${accessPointsId})`;
+  },
 };
