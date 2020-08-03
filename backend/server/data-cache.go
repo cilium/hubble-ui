31f67521a928a653b56d55f78eb7c0bb32b99c72
Renat Tuktarov <yandzeek@gmail.com>
2020-08-03 14:54:51 +0300

    fix: send updated link to client if verdict changed + domain types and cache refactor

---
+++ backend/server/data-cache.go
@@ -0,0 +1,71 @@
+package server
+
+import (
+	"github.com/cilium/hubble-ui/backend/domain/link"
+	"github.com/cilium/hubble-ui/backend/domain/service"
+)
+
+type dataCache struct {
+	services map[string]*service.Service
+	links    map[string]*link.Link
+}
+
+type cacheFlags struct {
+	Created bool
+	Updated bool
+	Deleted bool
+	Exists  bool
+}
+
+func newDataCache() *dataCache {
+	return &dataCache{
+		services: make(map[string]*service.Service),
+		links:    make(map[string]*link.Link),
+	}
+}
+
+func (c *dataCache) Drop() {
+	c.services = make(map[string]*service.Service)
+	c.links = make(map[string]*link.Link)
+}
+
+func (c *dataCache) UpsertService(svc *service.Service) *cacheFlags {
+	flags := new(cacheFlags)
+	svcId := svc.Id()
+
+	_, exists := c.services[svcId]
+	if !exists {
+		c.services[svcId] = svc
+		flags.Created = true
+	} else {
+		flags.Exists = true
+	}
+
+	return flags
+}
+
+func (c *dataCache) UpsertServiceLink(newLink *link.Link) *cacheFlags {
+	flags := new(cacheFlags)
+
+	currentLink, exists := c.links[newLink.Id]
+	if !exists {
+		c.links[newLink.Id] = newLink
+		flags.Created = true
+
+		return flags
+	}
+
+	if currentLink.Equals(newLink) {
+		flags.Exists = true
+		return flags
+	}
+
+	flags.Updated = true
+	// NOTE: the only thing that can differ is Verdict
+	c.links[newLink.Id] = newLink
+	return flags
+}
+
+func (cf *cacheFlags) Changed() bool {
+	return cf.Created || cf.Updated || cf.Deleted
+}
