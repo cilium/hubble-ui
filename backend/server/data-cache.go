cilium/hubble-ui

commit 79305bc39e09c0dde3d53b0c453d573b53dd64ac

fix: send updated link to client if verdict changed + domain types and cache refactor

backend/server/data-cache.go
@@ -1,0 +1,71 @@
+package server
+
+import (
+	"github.com/cilium/hubble-ui/backend/domain/link"
+	"github.com/cilium/hubble-ui/backend/domain/service"
+)
+
+type dataCache struct {
+	services map[string]*service.Service
+	links    map[string]*link.Link
+}
+
+type cacheFlags struct {
+	Created bool
+	Updated bool
+	Deleted bool
+	Exists  bool
+}
+
+func newDataCache() *dataCache {
+	return &dataCache{
+		services: make(map[string]*service.Service),
+		links:    make(map[string]*link.Link),
+	}
+}
+
+func (c *dataCache) Drop() {
+	c.services = make(map[string]*service.Service)
+	c.links = make(map[string]*link.Link)
+}
+
+func (c *dataCache) UpsertService(svc *service.Service) *cacheFlags {
+	flags := new(cacheFlags)
+	svcId := svc.Id()
+
+	_, exists := c.services[svcId]
+	if !exists {
+		c.services[svcId] = svc
+		flags.Created = true
+	} else {
+		flags.Exists = true
+	}
+
+	return flags
+}
+
+func (c *dataCache) UpsertServiceLink(newLink *link.Link) *cacheFlags {
+	flags := new(cacheFlags)
+
+	currentLink, exists := c.links[newLink.Id]
+	if !exists {
+		c.links[newLink.Id] = newLink
+		flags.Created = true
+
+		return flags
+	}
+
+	if currentLink.Equals(newLink) {
+		flags.Exists = true
+		return flags
+	}
+
+	flags.Updated = true
+	// NOTE: the only thing that can differ is Verdict
+	c.links[newLink.Id] = newLink
+	return flags
+}
+
+func (cf *cacheFlags) Changed() bool {
+	return cf.Created || cf.Updated || cf.Deleted
+}

backend/domain/link/link.go
@@ -1,0 +1,88 @@
+package link
+
+import (
+	"fmt"
+
+	pbFlow "github.com/cilium/cilium/api/v1/flow"
+	"github.com/cilium/hubble-ui/backend/domain/service"
+	"github.com/cilium/hubble-ui/backend/proto/ui"
+)
+
+type Link struct {
+	Id              string
+	SourceId        string
+	DestinationId   string
+	DestinationPort uint32
+
+	// TODO: this is bad; create domain types for this fields
+	Verdict    pbFlow.Verdict
+	IPProtocol ui.IPProtocol
+
+	ref *pbFlow.Flow
+}
+
+func FromFlowProto(f *pbFlow.Flow) *Link {
+	if f.L4 == nil || f.Source == nil || f.Destination == nil {
+		return nil
+	}
+
+	srcId, destId := service.IdsFromFlowProto(f)
+	destPort := uint32(0)
+	ipProtocol := ui.IPProtocol_UNKNOWN_IP_PROTOCOL
+
+	if tcp := f.L4.GetTCP(); tcp != nil {
+		destPort = tcp.DestinationPort
+		ipProtocol = ui.IPProtocol_TCP
+	}
+
+	if udp := f.L4.GetUDP(); udp != nil {
+		destPort = udp.DestinationPort
+		ipProtocol = ui.IPProtocol_UDP
+	}
+
+	if icmp4 := f.L4.GetICMPv4(); icmp4 != nil {
+		ipProtocol = ui.IPProtocol_ICMP_V4
+	}
+
+	if icmp6 := f.L4.GetICMPv6(); icmp6 != nil {
+		ipProtocol = ui.IPProtocol_ICMP_V6
+	}
+
+	protocolStr := ui.IPProtocol_name[int32(ipProtocol)]
+	linkId := fmt.Sprintf("%v %v %v:%v", srcId, protocolStr, destId, destPort)
+
+	return &Link{
+		Id:              linkId,
+		SourceId:        srcId,
+		DestinationId:   destId,
+		DestinationPort: destPort,
+		Verdict:         f.Verdict,
+		IPProtocol:      ipProtocol,
+
+		ref: f,
+	}
+}
+
+func (l *Link) ToProto() *ui.ServiceLink {
+	return &ui.ServiceLink{
+		Id:              l.Id,
+		SourceId:        l.SourceId,
+		DestinationId:   l.DestinationId,
+		DestinationPort: l.DestinationPort,
+		Verdict:         l.Verdict,
+		IpProtocol:      l.IPProtocol,
+	}
+}
+
+func (l *Link) Equals(rhs *Link) bool {
+	// NOTE: Id field is not participated here
+	return (l.SourceId == rhs.SourceId &&
+		l.DestinationId == rhs.DestinationId &&
+		l.DestinationPort == rhs.DestinationPort &&
+		l.Verdict == rhs.Verdict &&
+		l.IPProtocol == rhs.IPProtocol)
+}
+
+func (l *Link) IntoFlow() *pbFlow.Flow {
+	return l.ref
+}

backend/server/get-events.go
@@ -8,6 +8,8 @@ import (
 	"google.golang.org/grpc/status"
 
 	"github.com/cilium/cilium/api/v1/observer"
+	"github.com/cilium/hubble-ui/backend/domain/flow"
+	"github.com/cilium/hubble-ui/backend/domain/link"
 	"github.com/cilium/hubble-ui/backend/proto/ui"
 )
 
@@ -62,7 +64,7 @@ func (srv *UIServer) GetEvents(
 	flowDrain, flowErr, handleFlows := handleFlowStream(
 		flowSource,
 		eventsRequested,
-		srv.serviceCache,
+		srv.dataCache,
 	)
 
 	if handleFlows != nil {
@@ -110,9 +112,9 @@ func handleNsEvents(src chan *NSEvent, drain chan *ui.GetEventsResponse) {
 func handleFlowStream(
 	src FlowStream,
 	eventsRequested *eventFlags,
-	svcCache *serviceCache,
+	cache *dataCache,
 ) (chan *ui.GetEventsResponse, chan error, func()) {
-	svcCache.Drop()
+	cache.Drop()
 
 	if src == nil {
 		return nil, nil, nil
@@ -146,37 +148,51 @@ func handleFlowStream(
 				break
 			}
 
-			flow := flowResponse.GetFlow()
-			if flow == nil {
+			pbFlow := flowResponse.GetFlow()
+			if pbFlow == nil {
 				continue
 			}
+
+			f := flow.FromProto(pbFlow)
 
 			if eventsRequested.Flows {
 				drain <- &ui.GetEventsResponse{
 					Node:      flowResponse.NodeName,
 					Timestamp: flowResponse.Time,
-					Event:     &ui.GetEventsResponse_Flow{flow},
+					Event:     &ui.GetEventsResponse_Flow{pbFlow},
 				}
 			}
 
 			if eventsRequested.Services {
-				senderEvent, receiverEvent := svcCache.FromFlow(flow)
+				senderSvc, receiverSvc := f.BuildServices()
 
-				// Service state event (only EXISTS state is handled)
-				if senderEvent != nil {
+				flags := cache.UpsertService(senderSvc)
+				if flags.Changed() {
+					svc := senderSvc.ToProto()
+					senderEvent := eventResponseForService(pbFlow, svc, flags)
+
 					drain <- senderEvent
 				}
 
-				if receiverEvent != nil {
+				flags = cache.UpsertService(receiverSvc)
+				if flags.Changed() {
+					svc := receiverSvc.ToProto()
+					receiverEvent := eventResponseForService(pbFlow, svc, flags)
+
 					drain <- receiverEvent
 				}
 			}
 
 			if eventsRequested.ServiceLinks {
-				// Service Link event (only EXISTS state is handled)
-				linkEvent := svcCache.LinkFromFlow(flow)
+				serviceLink := link.FromFlowProto(pbFlow)
 
-				if linkEvent != nil {
+				if serviceLink == nil {
+					continue
+				}
+
+				flags := cache.UpsertServiceLink(serviceLink)
+				if flags.Changed() {
+					linkEvent := eventResponseForLink(serviceLink, flags)
 					drain <- linkEvent
 				}
 			}

backend/server/helpers.go
@@ -1,6 +1,10 @@
 package server
 
-import "github.com/cilium/hubble-ui/backend/proto/ui"
+import (
+	pbFlow "github.com/cilium/cilium/api/v1/flow"
+	"github.com/cilium/hubble-ui/backend/domain/link"
+	"github.com/cilium/hubble-ui/backend/proto/ui"
+)
 
 func getFlagsWhichEventsRequested(events []ui.EventType) *eventFlags {
 	flags := new(eventFlags)
@@ -28,4 +32,49 @@ func getFlagsWhichEventsRequested(events []ui.EventType) *eventFlags {
 
 func (ef *eventFlags) FlowsRequired() bool {
 	return ef.Flows || ef.Services || ef.ServiceLinks
+}
+
+func eventResponseForService(
+	f *pbFlow.Flow, svc *ui.Service, cflags *cacheFlags,
+) *ui.GetEventsResponse {
+	sstate := &ui.ServiceState{
+		Service: svc,
+		Type:    stateChangeFromCacheFlags(cflags),
+	}
+
+	return &ui.GetEventsResponse{
+		Node:      f.NodeName,
+		Timestamp: f.Time,
+		Event:     &ui.GetEventsResponse_ServiceState{sstate},
+	}
+}
+
+func eventResponseForLink(
+	l *link.Link, cflags *cacheFlags,
+) *ui.GetEventsResponse {
+	f := l.IntoFlow()
+	lstate := &ui.ServiceLinkState{
+		ServiceLink: l.ToProto(),
+		Type:        stateChangeFromCacheFlags(cflags),
+	}
+
+	return &ui.GetEventsResponse{
+		Node:      f.NodeName,
+		Timestamp: f.Time,
+		Event:     &ui.GetEventsResponse_ServiceLinkState{lstate},
+	}
+}
+
+func stateChangeFromCacheFlags(cflags *cacheFlags) ui.StateChange {
+	if cflags.Exists {
+		return ui.StateChange_EXISTS
+	} else if cflags.Created {
+		return ui.StateChange_ADDED
+	} else if cflags.Updated {
+		return ui.StateChange_MODIFIED
+	} else if cflags.Deleted {
+		return ui.StateChange_DELETED
+	}
+
+	return ui.StateChange_UNKNOWN_STATE_CHANGE
 }

backend/server/server.go
@@ -25,14 +25,14 @@ type UIServer struct {
 	hubbleAddr   string
 	hubbleClient observer.ObserverClient
 
-	k8s          kubernetes.Interface
-	serviceCache *serviceCache
+	k8s       kubernetes.Interface
+	dataCache *dataCache
 }
 
 func New(hubbleAddr string) *UIServer {
 	return &UIServer{
-		hubbleAddr:   hubbleAddr,
-		serviceCache: newServiceCache(),
+		hubbleAddr: hubbleAddr,
+		dataCache:  newDataCache(),
 	}
 }
 

backend/server/service-cache.go
@@ -1,127 +1,0 @@
-package server
-
-import (
-	"fmt"
-
-	pbFlow "github.com/cilium/cilium/api/v1/flow"
-	"github.com/cilium/hubble-ui/backend/domain/flow"
-	"github.com/cilium/hubble-ui/backend/domain/service"
-	"github.com/cilium/hubble-ui/backend/proto/ui"
-)
-
-type serviceCache struct {
-	services map[string]*ui.Service
-	links    map[string]*ui.ServiceLink
-}
-
-func newServiceCache() *serviceCache {
-	return &serviceCache{
-		services: make(map[string]*ui.Service),
-		links:    make(map[string]*ui.ServiceLink),
-	}
-}
-
-func (c *serviceCache) Drop() {
-	c.services = make(map[string]*ui.Service)
-	c.links = make(map[string]*ui.ServiceLink)
-}
-
-func (c *serviceCache) FromFlow(pbf *pbFlow.Flow) (
-	*ui.GetEventsResponse, *ui.GetEventsResponse,
-) {
-	var senderEvent, receiverEvent *ui.GetEventsResponse
-
-	f := flow.FromProto(pbf)
-	senderSvc, receiverSvc := f.BuildServices()
-	senderProto, receiverProto := senderSvc.ToProto(), receiverSvc.ToProto()
-
-	// TODO: check if updated data received from flow
-	_, exists := c.services[senderProto.Id]
-	if !exists {
-		c.services[senderProto.Id] = senderProto
-		senderEvent = eventResponseFromService(pbf, senderProto)
-	}
-
-	// TODO: check if updated data received from flow
-	_, exists = c.services[receiverProto.Id]
-	if !exists {
-		c.services[receiverProto.Id] = receiverProto
-		receiverEvent = eventResponseFromService(pbf, receiverProto)
-	}
-
-	return senderEvent, receiverEvent
-}
-
-func (c *serviceCache) LinkFromFlow(f *pbFlow.Flow) *ui.GetEventsResponse {
-	if f.L4 == nil || f.Source == nil || f.Destination == nil {
-		return nil
-	}
-
-	srcId, destId := service.IdsFromFlowProto(f)
-	destPort := uint32(0)
-	ipProtocol := ui.IPProtocol_UNKNOWN_IP_PROTOCOL
-
-	if tcp := f.L4.GetTCP(); tcp != nil {
-		destPort = tcp.DestinationPort
-		ipProtocol = ui.IPProtocol_TCP
-	}
-
-	if udp := f.L4.GetUDP(); udp != nil {
-		destPort = udp.DestinationPort
-		ipProtocol = ui.IPProtocol_UDP
-	}
-
-	if icmp4 := f.L4.GetICMPv4(); icmp4 != nil {
-		ipProtocol = ui.IPProtocol_ICMP_V4
-	}
-
-	if icmp6 := f.L4.GetICMPv6(); icmp6 != nil {
-		ipProtocol = ui.IPProtocol_ICMP_V6
-	}
-
-	protocolStr := ui.IPProtocol_name[int32(ipProtocol)]
-	linkId := fmt.Sprintf("%v %v %v:%v", srcId, protocolStr, destId, destPort)
-
-	// TODO: check if cached data should be updated and resend to client
-	_, ok := c.links[linkId]
-	if ok {
-		return nil
-	}
-
-	slink := &ui.ServiceLink{
-		Id:              linkId,
-		SourceId:        fmt.Sprintf("%v", srcId),
-		DestinationId:   fmt.Sprintf("%v", destId),
-		DestinationPort: destPort,
-		Verdict:         f.Verdict,
-		IpProtocol:      ipProtocol,
-	}
-
-	c.links[linkId] = slink
-
-	lstate := &ui.ServiceLinkState{
-		ServiceLink: slink,
-		Type:        ui.StateChange_EXISTS,
-	}
-
-	return &ui.GetEventsResponse{
-		Node:      f.NodeName,
-		Timestamp: f.Time,
-		Event:     &ui.GetEventsResponse_ServiceLinkState{lstate},
-	}
-}
-
-func eventResponseFromService(
-	f *pbFlow.Flow, svc *ui.Service,
-) *ui.GetEventsResponse {
-	sstate := &ui.ServiceState{
-		Service: svc,
-		Type:    ui.StateChange_EXISTS,
-	}
-
-	return &ui.GetEventsResponse{
-		Node:      f.NodeName,
-		Timestamp: f.Time,
-		Event:     &ui.GetEventsResponse_ServiceState{sstate},
-	}
-}

commit e689e69503bb9a1c1e1f50bfba7c5ff390c5bcbd

server: protect dataCache against concurrent access
As maps need to be protected against concurrent access we need to add a
mutex to protect those variables. Failing to do it so can cause the
program to crash with the following panic:

```
fatal error: concurrent map read and map write
```

Reported-by: Tam Mach <sayboras@yahoo.com>

Fixes: 79305bc39e09 ("fix: send updated link to client if verdict changed + domain types and cache refactor")
Signed-off-by: André Martins <andre@cilium.io>

backend/go.sum
@@ -427,6 +427,7 @@ github.com/pelletier/go-toml v1.2.0/go.mod h1:5z9KED0ma1S8pY6P1sdut58dfprrGBbd/9
 github.com/pelletier/go-toml v1.4.0 h1:u3Z1r+oOXJIkxqw34zVhyPgjBsm6X2wn21NWs/HfSeg=
 github.com/pelletier/go-toml v1.4.0/go.mod h1:PN7xzY2wHTK0K9p34ErDQMlFxa51Fk0OUruD3k1mMwo=
 github.com/peterbourgon/diskv v2.0.1+incompatible/go.mod h1:uqqh8zWWbv1HBMNONnaR/tNboyR3/BZd58JJSHlUSCU=
+github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 h1:q2e307iGHPdTGp0hoxKjt1H5pDo6utceo3dQVK3I5XQ=
 github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5/go.mod h1:jvVRKCrJTQWu0XVbaOlby/2lO20uSCHEMzzplHXte1o=
 github.com/pkg/errors v0.8.0/go.mod h1:bwawxfHBFNV+L2hUp1rHADufV3IMtnDRdf1r5NINEl0=
 github.com/pkg/errors v0.8.1/go.mod h1:bwawxfHBFNV+L2hUp1rHADufV3IMtnDRdf1r5NINEl0=
@@ -460,6 +461,7 @@ github.com/russross/blackfriday v1.5.2/go.mod h1:JO/DiYxRf+HjHt06OyowR9PTA263kcR
 github.com/russross/blackfriday/v2 v2.0.1/go.mod h1:+Rmxgy9KzJVeS9/2gXHxylqXiyQDYRxCVz55jmeOWTM=
 github.com/ryanuber/columnize v0.0.0-20160712163229-9b3edd62028f/go.mod h1:sm1tb6uqfes/u+d4ooFouqFdy9/2g9QGwK3SQygK0Ts=
 github.com/safchain/ethtool v0.0.0-20190326074333-42ed695e3de8/go.mod h1:Z0q5wiBQGYcxhMZ6gUqHn6pYNLypFAvaL3UvgZLR0U4=
+github.com/sasha-s/go-deadlock v0.2.1-0.20190427202633-1595213edefa h1:0U2s5loxrTy6/VgfVoLuVLFJcURKLH49ie0zSch7gh4=
 github.com/sasha-s/go-deadlock v0.2.1-0.20190427202633-1595213edefa/go.mod h1:F73l+cr82YSh10GxyRI6qZiCgK64VaZjwesgfQ1/iLM=
 github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529/go.mod h1:DxrIzT+xaE7yg65j358z/aeFdxmN0P9QXhEzd20vsDc=
 github.com/sergi/go-diff v1.0.0/go.mod h1:0CfEIISq7TuYL3j771MWULgwwjU+GofnZX9QAmXWZgo=
@@ -618,6 +620,7 @@ golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4/go.mod h1:RxMgew5VJxzue5/jJ
 golang.org/x/sync v0.0.0-20190227155943-e225da77a7e6/go.mod h1:RxMgew5VJxzue5/jJTE5uejpjVlOe/izrB70Jof72aM=
 golang.org/x/sync v0.0.0-20190412183630-56d357773e84/go.mod h1:RxMgew5VJxzue5/jJTE5uejpjVlOe/izrB70Jof72aM=
 golang.org/x/sync v0.0.0-20190423024810-112230192c58/go.mod h1:RxMgew5VJxzue5/jJTE5uejpjVlOe/izrB70Jof72aM=
+golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e h1:vcxGaoTs7kV8m5Np9uUNQin4BrLOthgV7252N8V+FwY=
 golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e/go.mod h1:RxMgew5VJxzue5/jJTE5uejpjVlOe/izrB70Jof72aM=
 golang.org/x/sys v0.0.0-20170830134202-bb24a47a89ea/go.mod h1:STP8DvDyc/dI5b8T5hshtkjS+E42TnysNCUPdjciGhY=
 golang.org/x/sys v0.0.0-20171017063910-8dbc5d05d6ed/go.mod h1:STP8DvDyc/dI5b8T5hshtkjS+E42TnysNCUPdjciGhY=

backend/server/data-cache.go
@@ -3,9 +3,12 @@ package server
 import (
 	"github.com/cilium/hubble-ui/backend/domain/link"
 	"github.com/cilium/hubble-ui/backend/domain/service"
+
+	"github.com/cilium/cilium/pkg/lock"
 )
 
 type dataCache struct {
+	lock.Mutex
 	services map[string]*service.Service
 	links    map[string]*link.Link
 }
@@ -25,6 +28,8 @@ func newDataCache() *dataCache {
 }
 
 func (c *dataCache) Drop() {
+	c.Mutex.Lock()
+	defer c.Mutex.Unlock()
 	c.services = make(map[string]*service.Service)
 	c.links = make(map[string]*link.Link)
 }
@@ -33,6 +38,8 @@ func (c *dataCache) UpsertService(svc *service.Service) *cacheFlags {
 	flags := new(cacheFlags)
 	svcId := svc.Id()
 
+	c.Mutex.Lock()
+	defer c.Mutex.Unlock()
 	_, exists := c.services[svcId]
 	if !exists {
 		c.services[svcId] = svc
@@ -47,6 +54,8 @@ func (c *dataCache) UpsertService(svc *service.Service) *cacheFlags {
 func (c *dataCache) UpsertServiceLink(newLink *link.Link) *cacheFlags {
 	flags := new(cacheFlags)
 
+	c.Mutex.Lock()
+	defer c.Mutex.Unlock()
 	currentLink, exists := c.links[newLink.Id]
 	if !exists {
 		c.links[newLink.Id] = newLink