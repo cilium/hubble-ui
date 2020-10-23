27d4152ebbadff42ad0aece94414d77b36dae2e5
Renat Tuktarov <yandzeek@gmail.com>
2020-10-23 12:48:29 +0300

    chore: commented / debug code removed

--- src/components/App/DataManager.ts
+++ src/components/App/DataManager.ts
@@ -98,11 +98,10 @@ export class DataManager extends EventEmitter<Events> {
     const store = this.store;
     const streamParams = store.controls.filters.clone().setNamespace(namespace);
 
-    // const secondaryFrame = store.currentFrame.filter(streamParams);
     const secondaryFrame = store
       .createFrame()
       .applyFrame(store.currentFrame, streamParams);
-    // debugger;
+
     store.pushFrame(secondaryFrame);
 
     const stream = this.api.v1.getEventStream(EventParamsSet.All, streamParams);
@@ -115,7 +114,6 @@ export class DataManager extends EventEmitter<Events> {
 
   public dropFilteringFrame() {
     if (this.filteringStream == null) return;
-    console.log('DataManager::dropFilteringFrame');
 
     this.filteringStream.stream.stop();
     this.offAllStreamEvents(this.filteringStream.stream);
