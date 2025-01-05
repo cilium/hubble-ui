fd6924bfc249242fc65fc233f6b02af552f405b6
rinsuki <428rinsuki+git@gmail.com>
2025-01-05 11:08:38 +0000

    fix: 404 Not Found error when baseUrl is not /

--- src/router/Routes.tsx
+++ src/router/Routes.tsx
@@ -10,21 +10,39 @@ export type Props = {
   RootComponent: JSX.Element;
 };
 
+const pathname = (href: string | undefined) => {
+  if (href == null) return undefined;
+  try {
+    return new URL(href).pathname;
+  } catch (e) {
+    console.error('Failed to determine base path', e);
+    return undefined;
+  }
+};
+
 export const Routes = function Routes(props: Props) {
   const createRouter = props.router.isInMemory ? createMemoryRouter : createBrowserRouter;
+  const opts = props.router.isInMemory
+    ? undefined
+    : {
+        basename: pathname(document.querySelector('base')?.href),
+      };
 
-  const router = createRouter([
-    {
-      path: '/',
-      element: props.RootComponent,
-      children: [
-        {
-          path: ApplicationPath.ServiceMap,
-          element: <ServiceMapApp />,
-        },
-      ],
-    },
-  ]);
+  const router = createRouter(
+    [
+      {
+        path: '/',
+        element: props.RootComponent,
+        children: [
+          {
+            path: ApplicationPath.ServiceMap,
+            element: <ServiceMapApp />,
+          },
+        ],
+      },
+    ],
+    opts,
+  );
 
   return <RouterProvider router={router} />;
 };
